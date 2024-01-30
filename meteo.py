89acc44d1d74aba32b0faac6f877086ce90c87b9
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-01-30 10:40:50 +0100

    ajout

--- meteo.py
+++ meteo.py
@@ -7,6 +7,7 @@ while True:
     devices = scanner.scan(timeout=3.0)
 
     for device in devices:
+        if (device.addr=="s6:c6:c7:39:a2:e8" or device.addr=="d6:1c:bf:b7:76:62" or device.addr=="d7:ef:13:27:15:29"):
         print(
             f"Device found {device.addr} ({device.addrType}), "
             f"RSSI={device.rssi} dB"
