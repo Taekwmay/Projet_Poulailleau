46563caa0f27f757a509bc7f7e62c2df2a4ffefc
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-01-30 11:02:24 +0100

    ajout

--- main.sh
+++ main.sh
@@ -5,10 +5,10 @@ sudo apt-get install -y python3-pip
 sudo apt-get install -y libglib2.0-dev
 sudo pip3 install flask
 sudo pip3 install bluepy
-mkdir ~/projmet
-cd ~/projmet
+mkdir /home/projmet
+cd /home/projmet
 wget https://github.com/Taekwmay/Projet_Poulailleau/archive/refs/heads/main.tar.gz
 tar -xzvf main.tar.gz
-cd ~/projmet/Projet_Poulailleau-main
+cd /home/projmet/Projet_Poulailleau-main
 chmod 766 meteo.py
-sudo python3 meteo.py 1>~/projmet/data.log 2>~/projmet/error.log
+sudo python3 meteo.py 2>&1 | tee -a data.log
