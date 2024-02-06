e184e50c7c552b144faf69d050f1f68ef4189fc3
sioulpas <157393090+sioulpas@users.noreply.github.com>
2024-02-06 09:38:16 +0100

    Update BDD.sh

--- BDD.sh
+++ BDD.sh
@@ -2,7 +2,9 @@ if ! dpkg -l | grep -q debconf-utils; then
     sudo apt-get update
     sudo apt-get install -y debconf-utils
 fi
+
 MYSQL_ROOT_PASSWORD="mariadb"
+DATABASE_NAME="nom_de_votre_base_de_donnees"
 
 echo "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD" > mysql-config.cfg
 echo "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" >> mysql-config.cfg
@@ -12,7 +14,10 @@ sudo debconf-set-selections < mysql-config.cfg
 sudo apt-get update
 sudo apt-get install -y default-mysql-server
 
+# Création de la base de données
+echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;" | sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD"
+
 rm mysql-config.cfg
 
 echo "MySQL a été installé avec le mot de passe root : $MYSQL_ROOT_PASSWORD"
-
+echo "La base de données $DATABASE_NAME a été créée."
