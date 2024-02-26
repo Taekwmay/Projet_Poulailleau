16b18478241c57289297243fedf63f16ad683cbc
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-02-26 14:59:43 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,9 +6,6 @@ sudo apt-get install -y libglib2.0-dev
 sudo pip3 install flask
 sudo pip3 install bluepy
 sudo pip3 install mysql-connector-python
-mkdir /tmp/projmet
-cd /tmp/projmet
-cd /tmp/projmet/Projet_Poulailleau-main
 chmod 766 meteo.py
 chmod 766 BDD.sh
 sudo ./BDD.sh
