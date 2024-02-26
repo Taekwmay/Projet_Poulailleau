f35481230928887a9d6484974330514a3d2b3e84
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-02-26 14:54:36 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,8 +8,6 @@ sudo pip3 install bluepy
 sudo pip3 install mysql-connector-python
 mkdir /tmp/projmet
 cd /tmp/projmet
-wget https://github.com/Taekwmay/Projet_Poulailleau/archive/refs/heads/main.tar.gz
-tar -xzvf main.tar.gz
 cd /tmp/projmet/Projet_Poulailleau-main
 chmod 766 meteo.py
 chmod 766 BDD.sh
