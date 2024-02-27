93bda3cbd3f391cdb88115338d0bdf1ed3624f29
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-02-27 15:07:27 +0100

    Update mainprinc.sh

--- mainprinc.sh
+++ mainprinc.sh
@@ -1,5 +1,6 @@
 mkdir /tmp/projmet
 cd /tmp/projmet
+sudo rm -f main.tar.gz
 wget https://github.com/Taekwmay/Projet_Poulailleau/archive/refs/heads/main.tar.gz
 tar -xzvf main.tar.gz
 cd /tmp/projmet/Projet_Poulailleau-main
