ab346978d35441d46bd71460632b476b6792294d
Moulbite <158071343+Moulbite@users.noreply.github.com>
2024-02-27 11:40:26 +0100

    Update and rename import.py to app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from app import Flask, render_template
+from flask import Flask, render_template
 from models import get_data_from_mysql
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data_demo1, data_demo2, data_demo3 = get_data_from_mysql()
+    data_demo1 = get_data_from_mysql(table_name="DEMO1")
+    data_demo2 = get_data_from_mysql(table_name="DEMO2")
+    data_demo3 = get_data_from_mysql(table_name="DEMO3")
     return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3)
 
 if __name__ == '__main__':
