586d876a55119d70ca71fc710e5ae29440de4efd
Moulbite <158071343+Moulbite@users.noreply.github.com>
2024-02-27 15:17:03 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 sudo raspi-config nonint do_ssh 0
 sudo apt-get install -y vsftpd python3-pip libglib2.0-dev
 sudo service vsftpd start
-sudo pip3 install bluepy  mysql-connector-python requests
+sudo pip3 install bluepy  mysql-connector-python
 chmod 766 meteo.py
 chmod 766 BDD.sh
 sudo ./BDD.sh
@@ -16,7 +16,7 @@ sudo mv /tmp/projmet/Projet_Poulailleau-main/50-server.cnf /etc/mysql/mariadb.co
 sudo service mysql restart
 python3 -m venv .venv
 . .venv/bin/activate
-pip install Flask mysql-connector-python
+pip install Flask mysql-connector-python requests
 flask --app app run --host=0.0.0.0 &
 sudo python3 meteo.py 2>&1
 
