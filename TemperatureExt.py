c2f02b56d5c9658daa7d51abc5e906fd8bb15bb7
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-02-27 15:44:01 +0100

    Update TemperatureExt.py

--- TemperatureExt.py
+++ TemperatureExt.py
@@ -12,13 +12,15 @@ def TempExt() :
     while True:
         formatted_csv_date = csv_date.strftime("%Y%m%d%H")
         url = f"https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/synop.{formatted_csv_date}.csv"
+        print(url)
         response = requests.get(url)
         if response.ok:
             data = response.content.decode('utf-8')  # Décode les octets en une chaîne de caractères
             csvfile = StringIO(data)  # Utilise StringIO pour traiter les données comme un fichier texte
             spamreader = csv.reader(csvfile, delimiter=';')
             for row in spamreader:
-                if row[0] == "07510":
+                print(row)
+                if row[0] == "07510" :
                     temperature_kelvin = float(row[7])  # Température en Kelvin
                     temperature_celsius = temperature_kelvin - 273.15  # Conversion en Celsius
                     print(f"Temperature : {temperature_celsius:.2f} °C")
