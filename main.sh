aa4e4ce93284be968f0db2d09b5d1b3a955e6049
Moulbite <158071343+Moulbite@users.noreply.github.com>
2024-02-27 11:38:21 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,6 @@ sudo apt-get install -y vsftpd
 sudo service vsftpd start
 sudo apt-get install -y python3-pip 
 sudo apt-get install -y libglib2.0-dev
-sudo pip3 install flask
 sudo pip3 install bluepy
 sudo pip3 install mysql-connector-python
 sudo pip3 install requests
@@ -11,3 +10,8 @@ chmod 766 meteo.py
 chmod 766 BDD.sh
 sudo ./BDD.sh
 sudo python3 meteo.py 2>&1
+python3 -m venv .venv
+. .venv/bin/activate
+pip install Flask
+pip install mysql-connector-python
+flask --app app run --host=0.0.0.0
