b10b087fc86f19cc1a2a8db87fd8323ee73daf21
Moulbite <158071343+Moulbite@users.noreply.github.com>
2024-02-27 16:06:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     data_demo1 = get_data_from_mysql(table_name="DEMO1")
     data_demo2 = get_data_from_mysql(table_name="DEMO2")
     data_demo3 = get_data_from_mysql(table_name="DEMO3")
-    return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, tempext=TempExt())
+    return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, tempext=round(TempExt(),2))
 
 if __name__ == '__main__':
     app.run(debug=True)
