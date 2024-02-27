752765dd727f9677966fcf41e1dfeb6f94ac3f05
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-02-27 16:05:18 +0100

    Update TemperatureExt.py

--- TemperatureExt.py
+++ TemperatureExt.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 def TempExt() :
     current_datetime = datetime.datetime.now()
     formatted_date = current_datetime.strftime("%Y%m%d%H")
-    csv_date = datetime.datetime(year=current_datetime.year, month=current_datetime.month, day=current_datetime.day+1, hour=current_datetime.hour // 3 * 3, minute=0)
+    csv_date = datetime.datetime(year=current_datetime.year, month=current_datetime.month, day=current_datetime.day, hour=current_datetime.hour // 3 * 3, minute=0)
 
     while True:
         formatted_csv_date = csv_date.strftime("%Y%m%d%H")
@@ -19,7 +19,6 @@ def TempExt() :
             csvfile = StringIO(data)  # Utilise StringIO pour traiter les données comme un fichier texte
             spamreader = csv.reader(csvfile, delimiter=';')
             for row in spamreader:
-                print(row)
                 if row[0] == "07510" :
                     temperature_kelvin = float(row[7])  # Température en Kelvin
                     temperature_celsius = temperature_kelvin - 273.15  # Conversion en Celsius
