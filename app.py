c0b615d63858b59f7ba75f26cc1a4db784696688
Moulbite <158071343+Moulbite@users.noreply.github.com>
2024-02-27 14:52:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from models import get_data_from_mysql
+from TemperatureExt import TempExt
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ def index():
     data_demo1 = get_data_from_mysql(table_name="DEMO1")
     data_demo2 = get_data_from_mysql(table_name="DEMO2")
     data_demo3 = get_data_from_mysql(table_name="DEMO3")
-    return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3)
+    return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, tempext=TempExt())
 
 if __name__ == '__main__':
     app.run(debug=True)
