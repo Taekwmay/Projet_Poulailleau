2a8dd63194625d0faed6188b1a13c6ca6d02603f
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-04-24 15:57:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ def index():
     data_demo3 = get_data_from_mysql(table_name="DEMO3")
     return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, tempext=round(TempExt(),2))
 
-    name_sensor1 = SensorInit(device_addr, sensor_name)
-    name_sensor2 = SensorInit(device_addr, sensor_name)
-    name_sensor3 = SensorInit(device_addr, sensor_name)
+    name_sensor1 = SensorInit("d6:1c:bf:b7:76:62", "DEMO1")
+    name_sensor2 = SensorInit("d6:c6:c7:39:a2:e8", "DEMO2")
+    name_sensor3 = SensorInit("d7:ef:13:27:15:29", "DEMO3")
     return render_template('index.html',
 
 if __name__ == '__main__':
