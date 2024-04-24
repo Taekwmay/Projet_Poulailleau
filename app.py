e89ea75fbc87deb0b00b41d41c658bc1e051aea7
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-04-24 16:47:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def index():
     data_demo2 = get_data_from_mysql(table_name="DEMO2")
     data_demo3 = get_data_from_mysql(table_name="DEMO3")
     return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, tempext=round(TempExt(),2))
-@app.route('/')
-def 
+@app.route('/update')
+def update_Sensor():
 #    name_sensor1 = SensorInit("d6:1c:bf:b7:76:62", "DEMO1")
 #    name_sensor2 = SensorInit("d6:c6:c7:39:a2:e8", "DEMO2")
 #    name_sensor3 = SensorInit("d7:ef:13:27:15:29", "DEMO3")
