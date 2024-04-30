8fcebf54ac2255959072a6f84d850ce2ba2c334b
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-04-30 10:59:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def index():
     data_demo1 = get_data_from_mysql(table_name="DEMO1")
     data_demo2 = get_data_from_mysql(table_name="DEMO2")
     data_demo3 = get_data_from_mysql(table_name="DEMO3")
-    return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, tempext=round(TempExt(),2))
+    return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, DEMO1 = "toto", tempext=round(TempExt(),2))
 
 @app.route('/change_name')
 def form_name():
