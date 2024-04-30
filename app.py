d495459bfed866094be126d1f072fac8646b418e
sioulpas <157393090+sioulpas@users.noreply.github.com>
2024-04-30 10:55:21 +0200

    v7

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import Flask, render_template, request
 from models import get_data_from_mysql
 from TemperatureExt import TempExt
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from my_models import Sensor, Base
+import mysql.connector
 
 app = Flask(__name__)
 
-# Connexion à la base de données
-engine = create_engine('mysql+pymysql://mariadb:mariadb@localhost/my_database')
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-
+# Fonction pour se connecter à la base de données et mettre à jour le nom du capteur
 def update_sensor_name(sensor_table, new_name):
-    session = DBSession()
-    sensor = session.query(Sensor).filter_by(sensor_name=sensor_table).first()
-    sensor.sensor_name = new_name
-    session.commit()
-    session.close()
+    # Connexion à la base de données
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="mariadb",
+        password="mariadb",
+        database="my_database"
+    )
+    cursor = conn.cursor()
 
+    # Exécution de la requête de mise à jour
+    sql = "UPDATE Sensors SET sensor_name = %s WHERE sensor_name = %s"
+    cursor.execute(sql, (new_name, sensor_table))
+
+    # Validation de la transaction et fermeture de la connexion
+    conn.commit()
+    conn.close()
 
 @app.route('/')
 def index():
@@ -36,14 +40,13 @@ def submit():
     sensor1 = request.form['sensor1']
     sensor2 = request.form['sensor2']
     sensor3 = request.form['sensor3']
-    
+
+    # Mise à jour des noms des capteurs dans la base de données
     update_sensor_name("DEMO1", sensor1)
     update_sensor_name("DEMO2", sensor2)
     update_sensor_name("DEMO3", sensor3)
 
-    # Vous pouvez également les enregistrer dans une base de données ou autre traitement nécessaire
+    return "Noms des capteurs mis à jour avec succès!"
 
-    return "Données soumises avec succès!"
-    
 if __name__ == '__main__':
-    app.run(host='192.168.233.153',debug=True)
+    app.run(host='192.168.233.153', debug=True)
