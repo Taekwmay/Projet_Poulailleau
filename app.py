2495fbbf253ca1002b67240e53d997525eb83649
sioulpas <157393090+sioulpas@users.noreply.github.com>
2024-04-30 10:00:42 +0200

    formulaire v2

--- app.py
+++ app.py
@@ -15,6 +15,21 @@ def index():
 @app.route('/change_name')
 def form_name():
     return render_template('change_name.html')
+
+@app.route('/change_name', methods=['POST'])
+def submit():
+    sensor1 = request.form['sensor1']
+    sensor2 = request.form['sensor2']
+    sensor3 = request.form['sensor3']
+
+    # Faire ce que vous voulez avec les données ici, par exemple, les afficher
+    print(f"Capteur 1: {sensor1}, MAC: {mac1}")
+    print(f"Capteur 2: {sensor2}, MAC: {mac2}")
+    print(f"Capteur 3: {sensor3}, MAC: {mac3}")
+
+    # Vous pouvez également les enregistrer dans une base de données ou autre traitement nécessaire
+
+    return "Données soumises avec succès!"
     
 if __name__ == '__main__':
     app.run(host='192.168.233.153',debug=True)
