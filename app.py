0d559160d4d8f8d84b511bce41df3bf51ffa9af8
sioulpas <157393090+sioulpas@users.noreply.github.com>
2024-04-30 11:42:21 +0200

    v20

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def index():
     data_demo1 = get_data_from_mysql(table_name="DEMO1")
     data_demo2 = get_data_from_mysql(table_name="DEMO2")
     data_demo3 = get_data_from_mysql(table_name="DEMO3")
-    return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, DEMO1 = recup('d6:1c:bf:b7:76:62'),DEMO2 = recup('d6:c6:c7:39:a2:e8'),DEMO3 = recup('d7:ef:13:27:15:29') tempext=round(TempExt(),2))
+    return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, DEMO1 = recup('d6:1c:bf:b7:76:62'), DEMO2 = recup('d6:c6:c7:39:a2:e8'),DEMO3 = recup('d7:ef:13:27:15:29'), tempext=round(TempExt(),2))
 
 @app.route('/change_name')
 def form_name():
