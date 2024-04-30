1f8b9b3eb37213f6db489fb36bbcdbb51cd677f1
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-04-30 09:26:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,5 @@ def index():
     data_demo3 = get_data_from_mysql(table_name="DEMO3")
     return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, tempext=round(TempExt(),2))
     
-@app.route('/update', methods=['POST'])
-def update_sensor_names():
-    global sensors
-    new_sensor_names = requests.form.getlist(SensorInit(device_addr, sensor_name))
-
-    # Met à jour les noms des capteurs avec les nouveaux noms
-    for i in range(len(sensors)):
-        sensors[i]['sensor_name'] = new_sensor_names[i]
-
-    # Redirige vers la page d'accueil après la mise à jour
-    return render_template('index.html', data_demo1=data_demo1, data_demo2=data_demo2, data_demo3=data_demo3, tempext=round(TempExt(),2), sensors=sensors)
-
 if __name__ == '__main__':
     app.run(host='192.168.233.153',debug=True)
