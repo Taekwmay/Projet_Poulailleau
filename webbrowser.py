7c5ce884f6bca5c116affa1dda3a74f23634a6ad
Taekwmay <158055395+Taekwmay@users.noreply.github.com>
2024-04-30 14:58:40 +0200

    Create webbrowser.py

---
+++ webbrowser.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+url = "http://192.168.233.153:5000"  # Remplacez ceci par l'URL de la page que vous souhaitez ouvrir
+
+# Ouvre la page web dans le navigateur par défaut
+webbrowser.open(url)
